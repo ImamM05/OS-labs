1f48637b1b608217b5c036cbd03c95ed234f6bf3
ImamM05 <imammishkat@gmail.com>
2026-03-01 12:24:00 -0500

    new changes

--- Assignment_1.c
+++ Assignment_1.c
@@ -51,34 +51,48 @@ void da_del(struct DynamicArray *arr){
 }
 
 int main(int argc, char* argv[]) {
-    char buf[BUFFER_AMT];
-
     if (argc < 2) {
         printf("Error: Missing filepath and txt file, ex: %s <filepath> <filename>\n", argv[0]);
         return -1;
     }
 
+    char buf[BUFFER_AMT];
+    struct DynamicArray extenList;
+    da_init(&extenList);
     int fd = open(argv[1], O_RDONLY);
 
     if (fd == -1) {
         perror("Error opening file");
         return -1;
     }
-    ssize_t bytesRead = read(fd, buf, 4);
+    ssize_t bytesRead;
+    unsigned int cursor = 0;
+    char token[CHAR_LENGTH]; // NEXT CREATE A COPY IN HEAP
+    while ((bytesRead = read(fd, buf, BUFFER_AMT)) > 0) {
+        for (unsigned int i = 0; i < bytesRead; i++) {
+            if (alnum(buf[i])) {
+                token[cursor++] = buf[i];
+            }
+            else if (cursor > 0) {
+                token[cursor] = '\0';
+                char *copy = malloc(strlen(token)+1);
+                if (copy == NULL) {
+                    perror("Error while saving word to list.");
+                    return -1;
+                }
+                strcpy(copy, token);
+                da_insert_back(&extenList, copy);
+                cursor = 0;
+            }
+        }
+    }
     if (bytesRead == -1) {
-        perror("Error reading file %s", argv[1]);
+        perror("Error reading file.");
         return -1;
     }
     
-
-
-
-    free(buf);
     close(fd);
+    da_del(&extenList);
     return 0;
     
-}
-
-void addToList(const char** const list, const char* extension) {
-
 }
